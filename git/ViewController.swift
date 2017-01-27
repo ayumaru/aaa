a12a63ff42109f4f503f9dd560266fe7fdec5f63
ayumaru <ths40374@gmail.com>
2017-01-27 20:27:39 +0900

    テス

--- git/ViewController.swift
+++ git/ViewController.swift
@@ -12,8 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // aaaaaaaa
         print("github")
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
